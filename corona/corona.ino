7ffd754c46f3d4b1bed4aa61efb51650ae7900f6
brandon <btherr2003@gmail.com>
2020-06-20 13:20:05 -0500

    fix github??

---
+++ corona/corona.ino
@@ -0,0 +1,70 @@
+#include <Servo.h>
+
+//declare pins
+//fuck this orginization
+const int servoPin = 3;
+
+const int motor1Pwm = 11;
+const int motor2Pwm = 10;
+
+const int motor1Dir1 = 9;
+const int motor1Dir2 = 8;
+const int motor2Dir1 = 12;
+const int motor2Dir2 = 13;
+
+
+Servo servo;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(servoPin, OUTPUT);
+  pinMode(motor1Pwm, OUTPUT);
+  pinMode(motor2Pwm, OUTPUT);
+  pinMode(motor1Dir1, OUTPUT);
+  pinMode(motor1Dir2, OUTPUT);
+  pinMode(motor2Dir1, OUTPUT);
+  pinMode(motor2Dir2, OUTPUT);
+
+  //startup sequence
+  servo.attach(servoPin);
+  servo.write(0);
+  delay(1000);
+
+  digitalWrite(motor1Dir1, HIGH);
+  digitalWrite(motor2Dir1, HIGH);
+  digitalWrite(motor1Dir2, LOW);
+  digitalWrite(motor2Dir2, LOW);
+  analogWrite(motor1Pwm, 512);
+  analogWrite(motor2Pwm, 512);
+  delay(1000);
+  
+  analogWrite(motor1Pwm, 0);
+  analogWrite(motor2Pwm, 0);
+  delay(1000);
+
+  digitalWrite(motor1Dir1, LOW);
+  digitalWrite(motor2Dir1, LOW);
+  digitalWrite(motor1Dir2, HIGH);
+  digitalWrite(motor2Dir2, HIGH);
+  analogWrite(motor1Pwm, 512);
+  analogWrite(motor2Pwm, 512);
+  delay(1000);
+
+  analogWrite(motor1Pwm, 0);
+  analogWrite(motor2Pwm, 0);
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available) {
+     char state = Serial.read();
+     if(state == "stop") {
+      analogWrite(motor1Pwm, 0);
+      analogWrite(motor2Pwm, 0);
+      servo.write(90);
+     }
+  }
+}
